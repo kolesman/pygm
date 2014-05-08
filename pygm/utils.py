e0ee4b9619d700d1614c20bfb0c80e9b309a55d3
Alexander Kolesnikov <akolesnikov@l0241046.ista.local>
2014-05-08 17:51:18 +0200

    refactoring

--- pygm/utils.py
+++ pygm/utils.py
@@ -93,33 +93,3 @@ def dictMult(d, alpha):
 
 def dictDot(d1, d2, default=0):
     return sum(d1[key] * d2[key] for key in set(d1) & set(d2))
-
-
-def getUpdateFromProjectionModel(m):
-
-    update = {}
-
-    for i, duals in enumerate(m._duals):
-        for members, variables in duals.items():
-            if len(members) == 1:
-                for u, var in enumerate(variables):
-                    update[(i, members, (u, ))] = var.x
-            if len(members) == 2:
-                for u, var_line in enumerate(variables):
-                    for v, var in enumerate(var_line):
-                        update[(i, members, (u, v))] = var.x
-
-    return update
-
-
-def getSolutionFromLPModel(m):
-
-    solution = {}
-
-    for members, var_list in m._variables.items():
-        if len(members) == 1:
-            solution[members[0]] = np.argmax([v.x for v in var_list])
-
-    solution = map(lambda x: x[1], sorted(solution.items()))
-
-    return solution
